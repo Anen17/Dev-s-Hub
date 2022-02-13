2598bb2cbd6bdf2edc7eb762124cd03547bc393d
Anen17 <80644641+Anen17@users.noreply.github.com>
2022-02-12 22:45:15 -0500

    Update OurGui.lua

--- OurGui.lua
+++ OurGui.lua
@@ -7,7 +7,7 @@ local ThirdPage = MainUI.AddPage("World")
 local FourthPage = MainUI.AddPage("Scripts")
 local FithPage = MainUI.AddPage("Settings")
 
-local Label1 = FirstPage.AddLabel("Home 1")
+local Label1 = FirstPage.AddLabel("Main")
 
 local Toggle1 = FirstPage.AddToggle("InfiniteJump", false, function(Value)
   _G.infinjump = Value
@@ -105,6 +105,22 @@ local Button1 = FirstPage.AddButton("Key Teleport (v)", function()
 	})
 end)
 
+local Button1 = FirstPage.AddButton("Anti-Afk", function()
+	local VirtualUser=game:service'VirtualUser'
+    game:service'Players'.LocalPlayer.Idled:connect(function()
+    VirtualUser:CaptureController()
+    VirtualUser:ClickButton2(Vector2.new())
+end)
+    game.StarterGui:SetCore("SendNotification", {
+	Title = "Experience";
+	Text = "Anti-Afk Injected";
+	Icon = "rbxassetid://6034996698";
+	Duration = 10;
+})
+end)
+
+local Label2 = FirstPage.AddLabel("Character Editor")
+
 local Slider1 = FirstPage.AddSlider("WalkSpeed", {Min = 0, Max = 255, Def = 16}, function(Value)
 	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
 end)
@@ -200,6 +216,89 @@ end)
 
 local Button4 = FourthPage.AddButton("FunkyFriday Autoplayer", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/wally-rblx/funky-friday-autoplay/main/main.lua",true))()
+	    game.StarterGui:SetCore("SendNotification", {
+		Title = "Dev's Hub";
+		Text = "Scripts Injected";
+		Icon = "rbxassetid://6034996698";
+		Duration = 10;
+	})
+end)
+
+local Button4 = FourthPage.AddButton("Chat Spy", function()
+   --This script reveals ALL hidden messages in the default chat
+--chat "/spy" to toggle!
+enabled = true
+--if true will check your messages too
+spyOnMyself = true
+--if true will chat the logs publicly (fun, risky)
+public = false
+--if true will use /me to stand out
+publicItalics = true
+--customize private logs
+privateProperties = {
+	Color = Color3.fromRGB(0,255,255); 
+	Font = Enum.Font.SourceSansBold;
+	TextSize = 18;
+}
+--////////////////////////////////////////////////////////////////
+local StarterGui = game:GetService("StarterGui")
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+local saymsg = game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest")
+local getmsg = game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("OnMessageDoneFiltering")
+local instance = (_G.chatSpyInstance or 0) + 1
+_G.chatSpyInstance = instance
+
+local function onChatted(p,msg)
+	if _G.chatSpyInstance == instance then
+		if p==player and msg:lower():sub(1,4)=="/spy" then
+			enabled = not enabled
+			wait(0.3)
+			privateProperties.Text = "{SPY "..(enabled and "EN" or "DIS").."ABLED}"
+			StarterGui:SetCore("ChatMakeSystemMessage",privateProperties)
+		elseif enabled and (spyOnMyself==true or p~=player) then
+			msg = msg:gsub("[\n\r]",''):gsub("\t",' '):gsub("[ ]+",' ')
+			local hidden = true
+			local conn = getmsg.OnClientEvent:Connect(function(packet,channel)
+				if packet.SpeakerUserId==p.UserId and packet.Message==msg:sub(#msg-#packet.Message+1) and (channel=="All" or (channel=="Team" and public==false and Players[packet.FromSpeaker].Team==player.Team)) then
+					hidden = false
+				end
+			end)
+			wait(1)
+			conn:Disconnect()
+			if hidden and enabled then
+				if public then
+					saymsg:FireServer((publicItalics and "/me " or '').."{SPY} [".. p.Name .."]: "..msg,"All")
+				else
+					privateProperties.Text = "{SPY} [".. p.Name .."]: "..msg
+					StarterGui:SetCore("ChatMakeSystemMessage",privateProperties)
+				end
+			end
+		end
+	end
+end
+
+for _,p in ipairs(Players:GetPlayers()) do
+	p.Chatted:Connect(function(msg) onChatted(p,msg) end)
+end
+Players.PlayerAdded:Connect(function(p)
+	p.Chatted:Connect(function(msg) onChatted(p,msg) end)
+end)
+privateProperties.Text = "{SPY "..(enabled and "EN" or "DIS").."ABLED}"
+StarterGui:SetCore("ChatMakeSystemMessage",privateProperties)
+local chatFrame = player.PlayerGui.Chat.Frame
+chatFrame.ChatChannelParentFrame.Visible = true
+chatFrame.ChatBarParentFrame.Position = chatFrame.ChatChannelParentFrame.Position+UDim2.new(UDim.new(),chatFrame.ChatChannelParentFrame.Size.Y)
+	    game.StarterGui:SetCore("SendNotification", {
+		Title = "Dev's Hub";
+		Text = "Scripts Injected";
+		Icon = "rbxassetid://6034996698";
+		Duration = 10;
+	})
+end)
+
+local Button4 = FourthPage.AddButton("Remote Spy", function()
+    loadstring(game:HttpGet("https://github.com/exxtremestuffs/SimpleSpySource/raw/master/SimpleSpy.lua"))()
 	    game.StarterGui:SetCore("SendNotification", {
 		Title = "Dev's Hub";
 		Text = "Scripts Injected";
